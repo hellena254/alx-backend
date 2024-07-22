55572dac62581abc28c8966b4434d1cd7c0768fa
hellena254 <hellenah243@gmail.com>
2024-07-22 00:42:44 -0700

    add simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Simple Helper Function
+"""
+
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int: int]:
+    """
+    Takes two integer arguments page and page_size
+    Return a tuple of size two containing a start index and an end index
+    Page numbers are 1-indexed
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
