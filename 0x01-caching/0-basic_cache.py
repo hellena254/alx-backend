0418cc028353c722b7d0c44067d822c645738630
hellena254 <hellenah243@gmail.com>
2024-07-25 06:42:56 -0700

    BasicCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Basic Cache module
+"""
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache that inherits from BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """
+        Initialize the BasicCache.
+	"""
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Add an item to the cache.
+	"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache.
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
