b84b4bda47c9fed8aba39c7b42eb8da6fa114282
hellena254 <hellenah243@gmail.com>
2024-07-30 00:26:41 -0700

    add babel config

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#/usr/bin/env python3
+
+"""
+Basic Flask app setup with babel configf for i18n
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Config class for the Flask app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Render the index page
+    """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
